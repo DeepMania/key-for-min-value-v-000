e2f9bfd33ee0b0b5fd331f789908f05d38db107c
Yana Ustsinovich <yustinovich@icloud.com>
2018-10-08 01:16:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,12 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  name_hash.each do |name, age|
-    if lowest_value == nil || age < lowest_value
-      lowest_value = age
-      lowest_key = name_hash
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
+  end
     lowest_key
   end
 end
